7fb79cc70eccb8df39b067e08b35578936d2a641
GhostErrorCode <2512908103@qq.com>
2025-11-26 18:32:13 +0800

    命令/通知更改

---
+++ WPFStudy/Command/ViewModels/Base/CmdMainViewModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WPFStudy.Command.ViewModels.Base
+{
+    public class CmdMainViewModelBase : INotifyPropertyChanged
+    {
+        // 这个类是为了实现 然后通知UI元素更改他已经绑定的值
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // 写一个类，需要自己去调用
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            // [CallerMemberName] string propertyName = "" 这是一个C#编译器特性，主要用于自动获取调用方法的成员名称，在WPF的MVVM模式中特别有用
+            // 如果不为空，则调用
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ WPFStudy/Command/Views/Pages/CmdMainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPFStudy.Command.ViewModels.Pages;
+
+namespace WPFStudy.Command.Views.Pages
+{
+    /// <summary>
+    /// CmdMainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class CmdMainWindow : Window
+    {
+        public CmdMainWindow()
+        {
+            InitializeComponent();
+
+            // 载入上下文
+            // 载入ViewModel
+            this.DataContext = new CmdMainViewModel();
+        }
+    }
+}
