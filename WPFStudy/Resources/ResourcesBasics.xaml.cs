d34d70d6f7b1095285baeb0fdadc5cbea1087a4c
GhostErrorCode <2512908103@qq.com>
2025-11-28 18:24:08 +0800

    资源基础

---
+++ WPFStudy/Resources/ResourcesBasics.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFStudy.Resources
+{
+    /// <summary>
+    /// ResourcesBasics.xaml 的交互逻辑
+    /// </summary>
+    public partial class ResourcesBasics : Window
+    {
+        public ResourcesBasics()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Resources["SolidColor"] = new SolidColorBrush(System.Windows.Media.Color.FromRgb(0, 0, 255));
+
+            // 获取资源
+            var style = App.Current.FindResource("DefaultButtonStyle");
+        }
+    }
+}
