d5591b1a80854d829c56f0205a9cab4d883ec491
GhostErrorCode <2512908103@qq.com>
2026-02-28 11:27:45 +0800

    主题及自定义颜色切换服务

---
+++ WPFUITEST/WpfUiTest.App/Services/Implements/CustomThemeService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using Wpf.Ui.Appearance;
+using WpfUiTest.Core.Services.Interfaces;
+using WpfUiTest.Shared.Enums;
+using WpfUiTest.Shared.Extensions;
+
+namespace WpfUiTest.App.Services.Implements
+{
+    // 自定义应用主题服务接口实现类
+    public class CustomThemeService : ICustomThemeService
+    {
+        // 配置项：实际项目名称
+        private string _projectName = "WpfUiTest.App";
+        private string _customThemeBasePath = "Resources/Colors/Themes/";
+
+        // 字段: 资源字典上下文
+        private Collection<ResourceDictionary> _appResources = Application.Current.Resources.MergedDictionaries;
+
+        public void InitTheme(Theme targetTheme)
+        {
+            this.SwitchTheme(targetTheme);
+        }
+
+        public void SwitchTheme(Theme targetTheme)
+        {
+            // 1. 删除旧字典，查找资源字典集合里的自定义主题资源
+            ResourceDictionary? oldResource = this._appResources.FirstOrDefault(d =>
+                d.Source?.ToString().EndsWith("pack://application:,,,/WpfUiTest.App;component/Resources/Colors/Themes/Dark.xaml") == true ||
+                d.Source?.ToString().EndsWith("pack://application:,,,/WpfUiTest.App;component/Resources/Colors/Themes/Light.xaml") == true ||
+                d.Source?.ToString().EndsWith("pack://application:,,,/WpfUiTest.App;component/Resources/Colors/Themes/HighContrast.xaml") == true);
+            // 如果找到了且不为null就移除他
+            if (oldResource != null) this._appResources.Remove(oldResource);
+
+            // 2. 拼接绝对路径
+            // 根据切换后的目标主题找到需要的自定义主题资源字典文件
+            string fileName = targetTheme.EnumThemeToEnglishStringTheme() + ".xaml";
+            // 完整的路径
+            string fullPath = $"pack://application:,,,/{this._projectName};component/{this._customThemeBasePath}{fileName}";
+            // 新的资源字典
+            ResourceDictionary newResource = new ResourceDictionary { Source = new Uri(fullPath, UriKind.Absolute) };
+
+            // 3. 添加新字典
+            this._appResources.Add(newResource);
+
+            // 4.强制刷新资源字典集合
+            // 重置资源字典引用，触发DynamicResource重新解析
+            //Application.Current.Resources.MergedDictionaries.Contains(newResource);
+
+            // 5.应用WPFUI主题
+            ApplicationThemeManager.Apply(targetTheme.EnumThemeToApplicationTheme());
+        }
+    }
+}
