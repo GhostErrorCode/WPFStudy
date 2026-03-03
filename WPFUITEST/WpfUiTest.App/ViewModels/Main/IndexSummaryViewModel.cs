b0109e0d3bc1e7afc1b91ee654aa97a14ad7a3c9
GhostErrorCode <2512908103@qq.com>
2026-03-03 14:27:06 +0800

    首页汇总功能

--- WPFUITEST/WpfUiTest.App/ViewModels/Main/IndexSummaryViewModel.cs
+++ WPFUITEST/WpfUiTest.App/ViewModels/Main/IndexSummaryViewModel.cs
@@ -1,9 +1,14 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
 using WpfUiTest.App.ViewModels.Enums;
+using WpfUiTest.Core.DTOs;
+using WpfUiTest.Core.Services.Interfaces;
 using WpfUiTest.Shared.Base;
+using WpfUiTest.Shared.Utilities;
 
 namespace WpfUiTest.App.ViewModels.Main
 {
@@ -11,6 +16,13 @@ namespace WpfUiTest.App.ViewModels.Main
     public class IndexSummaryViewModel : BaseViewModel
     {
         // ==================== 字段、属性、命令 ====================
+        // 字段：首页汇总服务
+        private readonly ISummaryService _summaryService;
+        // 字段：IMessenger服务
+        private readonly IMessenger _messenger;
+        // 字段：ILogger服务
+        private readonly ILogger<IndexSummaryViewModel> _logger;
+
         // 属性：首页汇总
         private ObservableCollection<IndexSummaryItemViewModel> _indexSummaryItems;
         public ObservableCollection<IndexSummaryItemViewModel> IndexSummaryItems
@@ -19,11 +31,14 @@ namespace WpfUiTest.App.ViewModels.Main
             set { SetProperty(ref _indexSummaryItems, value); }
         }
 
-
         // ==================== 构造函数 ====================
-        public IndexSummaryViewModel()
+        public IndexSummaryViewModel(ISummaryService summaryService, IMessenger messenger, ILogger<IndexSummaryViewModel> logger)
         {
             // 初始化字段
+            this._summaryService = summaryService;
+            this._messenger = messenger;
+            this._logger = logger;
+
             // 初始化属性
             this._indexSummaryItems = new ObservableCollection<IndexSummaryItemViewModel>()
             {
@@ -36,7 +51,58 @@ namespace WpfUiTest.App.ViewModels.Main
 
             // 初始化命令
         }
+        // 初始化首页汇总数据
+
 
         // ==================== 方法 ====================
+        // 私有方法：初始化首页汇总数据
+        private async Task Init()
+        {
+            try
+            {
+                // ========== 初始化汇总数据 ==========
+                // 每次导航进入都获取一次汇总数据
+                ServiceResult<SummaryDto> summaryResult = await this._summaryService.GetSummaryAsync();
+                if (summaryResult.IsSuccess && summaryResult.Data != null)
+                {
+                    foreach(IndexSummaryItemViewModel item in this.IndexSummaryItems)
+                    {
+                        item.Content = item.SummaryType switch
+                        {
+                            IndexSummaryType.ToDoTotal => summaryResult.Data.ToDoTotal.ToString(), // 待办总数转字符串
+                            IndexSummaryType.Completed => summaryResult.Data.TodoCompleted.ToString(), // 已完成数
+                            IndexSummaryType.CompletionRate => summaryResult.Data.TodoCompletionRate, // 完成率（直接用Dto里的%格式）
+                            IndexSummaryType.MemoTotal => summaryResult.Data.MemoTotal.ToString(), // 备忘录总数
+                            IndexSummaryType.Custom => summaryResult.Data.Custom.ToString(), // 自定义数值
+                            _ => "错误" // 兜底，避免漏枚举
+                        };
+                    }
+                    // 输出日志
+                    this._logger.LogInformation("首页（IndexView）汇总数据已获取");
+                }
+                else
+                {
+                    foreach (IndexSummaryItemViewModel item in this.IndexSummaryItems) { item.Content = "获取失败"; }
+                    // 输出日志
+                    this._logger.LogWarning("首页（IndexView）汇总数据获取失败，异常信息：{Message}", summaryResult.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                // 输出日志
+                this._logger.LogError("首页（IndexView）汇总数据获取失败，处理首页汇总数据中出现意外的严重错误! 异常信息：{ex}", ex);
+            }
+        }
+
+
+
+
+
+        public override async Task OnNavigatedToAsync()
+        {
+            // 调用自己的初始化方法
+            await this.Init();
+            await base.OnNavigatedToAsync();
+        }
     }
 }
---
+++ WPFUITEST/WpfUiTest.Core/Services/Interfaces/ISummaryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WpfUiTest.Core.DTOs;
+using WpfUiTest.Shared.Utilities;
+
+namespace WpfUiTest.Core.Services.Interfaces
+{
+    // 首页汇总数据接口
+    public interface ISummaryService
+    {
+        // 数据汇总，统计待办事项总量、待办事项完成数量、待办事项完成比例、备忘录总数等
+        public Task<ServiceResult<SummaryDto>> GetSummaryAsync();
+    }
+}
